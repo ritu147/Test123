a14ccb0d2abc2ce630dc9af9dd4fb23062b1c806
mailtoritu@gmail.com <ritupal992@gmail.com>
2024-04-30 00:40:06 +0530

    added same functionalities from main branch

--- file_1.c
+++ file_1.c
@@ -1,1 +1,33 @@
 This is a new file in new branch. Adding conflicts.
+
+#include <stdio.h>
+int main() {
+
+  int i, n;
+
+  // initialize first and second terms
+  int t1 = 0, t2 = 1;
+
+  // initialize the next term (3rd term)
+  int nextTerm = t1 + t2;
+
+  // get no. of terms from user
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+
+  // print the first two terms t1 and t2
+  printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+  // print 3rd to nth terms
+  for (i = 3; i <= n; ++i) {
+    printf("%d, ", nextTerm);
+    printf("Hello World");
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
+
+  return false;
+  return 0;
+}
+
